baf3047f8330f945e3abd29ee5519c090f2ba4e4
David Pitcher <114188266+davidvpitcher@users.noreply.github.com>
2025-05-09 09:30:19 -0700

    Create ContactPage.vue
    Standard contact page

---
+++ src/views/ContactPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page">
+    <h1>Contact Us</h1>
+    <p>If you have any questions, feel free to reach out to David Pitcher at <strong>mortalitycoregames@gmail.com</strong>.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactPage'
+}
+</script>
+
+<style scoped>
+.page h1 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+.page p {
+  line-height: 1.6;
+}
+</style>
