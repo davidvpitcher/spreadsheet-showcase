2384135952409f0df0b3a158682db6d01f9d7e33
David Pitcher <114188266+davidvpitcher@users.noreply.github.com>
2025-05-09 09:31:56 -0700

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/css/global-handsontable-styles.css';
+
+createApp(App).use(router).mount('#app')
